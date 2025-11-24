f572f8eab300f24cbf14f6eedbdb51faeb9a879c
Uni <justinlee3318@gmail.com>
2025-11-23 21:51:25 -0800

    Added a delete function for tripParticipants

--- backend/api-data-systems/controllers/tripParticipants.js
+++ backend/api-data-systems/controllers/tripParticipants.js
@@ -33,9 +33,28 @@ const getParticipantByTripId = async (req, res) => {
     }
 }
 
+const deleteParticipant = async (req, res) => {
+    try{
+        const { tripId } = req.params;
+        const { userId } = req.params;
+        const {data, error } = await supabase
+         .from('tripParticipants')
+         .delete()
+         .eq('trip', tripId)
+         .eq('user', userId)
+         .select();
+        
+        if (error) return res.status(400).json({ error: error.message});
+        return res.status(200).json(data);
+
+    } catch(err){
+        console.error('Server error: ', err);
+        return res.sendStatus(500);
+    }
+}
 
 module.exports = {
     createParticipant,
     getParticipantByTripId,
-    
+    deleteParticipant,
 }
--- backend/api-data-systems/routes/tripParticipants.js
+++ backend/api-data-systems/routes/tripParticipants.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
     createParticipant,
-    getParticipantByTripId
+    getParticipantByTripId,
+    deleteParticipant,
 } = require('../controllers/tripParticipants.js');
 
 router.post('/', createParticipant);
 router.get('/:tripId', getParticipantByTripId);
+router.delete('/:tripId/:userId', deleteParticipant);
 
 module.exports = router;
